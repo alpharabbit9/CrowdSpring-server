e8a81f2a9a1415abefb1dd0692899836039c23e6
alpharabbit9 <alpharabbit74@gmail.com>
2025-03-22 15:55:27 +0600

    added final touches and deployed

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT | 5000;
 
-app.use(cors());
+app.use(cors({ origin: '*' })); 
 app.use(express.json());
 
 
 
 
-const uri = "mongodb+srv://crowdfunding:D6S810fjbq9v1yv0@cluster0.chblh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.chblh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,9 +25,9 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.connect();
+        // // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
 
         const campaignCollection = client.db('CrowndFundingDB').collection('campaigns')
         const donationCollection = client.db('CrowndFundingDB').collection('doantions')
@@ -47,10 +48,10 @@ async function run() {
             }
         });
 
-        
-        app.get('/campaigns/:id' , async(req,res) =>{
+
+        app.get('/campaigns/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id : new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await campaignCollection.findOne(query);
             res.send(result);
         })
@@ -58,7 +59,7 @@ async function run() {
         app.get('/myCampaign', async (req, res) => {
             const email = req.query.email;  // Get email from query
             if (!email) return res.status(400).json({ error: "Email is required" });
-        
+
             try {
                 const campaign = await campaignCollection.find({ email }).toArray();  // Filter by email
                 res.json(campaign);
@@ -68,8 +69,8 @@ async function run() {
             }
         });
 
-        app.post('/campaigns',async (req  , res)=>{
-            const campaign =req.body;
+        app.post('/campaigns', async (req, res) => {
+            const campaign = req.body;
             const result = await campaignCollection.insertOne(campaign);
             res.send(result);
         })
@@ -77,7 +78,20 @@ async function run() {
 
         // DonationAmount
 
-        app.post('/donationAmount' , async(req,res)=>{
+        app.get('/donationAmount', async (req, res) => {
+            const email = req.query.email;  // Get email from query
+            if (!email) return res.status(400).json({ error: "Email is required" });
+
+            try {
+                const campaign = await donationCollection.find({ donor_email: email }).toArray();  // Filter by email
+                res.json(campaign);
+            } catch (error) {
+                console.error("Error fetching tasks:", error);
+                res.status(500).json({ error: "Internal Server Error" });
+            }
+        });
+
+        app.post('/donationAmount', async (req, res) => {
 
             const donation = req.body;
             const result = await donationCollection.insertOne(donation);
